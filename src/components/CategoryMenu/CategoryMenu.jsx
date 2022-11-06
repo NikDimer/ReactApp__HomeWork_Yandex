bcdfc7e2661b77bcc9434c96434cf5a1cb98410a
NikDimer <60222621+NikDimer@users.noreply.github.com>
2022-11-06 23:30:06 +0300

    v1.1

--- src/components/CategoryMenu/CategoryMenu.jsx
+++ src/components/CategoryMenu/CategoryMenu.jsx
@@ -4,9 +4,8 @@ import styles from './styles.module.css';
 
 export const CategoryMenu = (props) => {
     return <nav className={classnames(styles.mainNavigation)}>
-            <h2 className={classnames(styles.mainNavigationTitle)}>Художественная литература</h2>
                 {
-                    props.booksShop.map((category) => <button className={classnames(styles.mainNavigationItem)} key={category.id} onClick={() => props.setCategory(category)}>{category.name}</button>)
+                    props.booksShop.map((category) => <button className={classnames(styles.mainNavigationItem, {[styles.activeCategory]:category.id === props.actCategory.id})} key={category.id} onClick={() => props.setCategory(category)}>{category.name}</button>)
                 }
       </nav>
     }
