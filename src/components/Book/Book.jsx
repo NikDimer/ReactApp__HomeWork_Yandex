29ec063cabffe49cc395c3895b3aae55f20b61b5
NikDimer <60222621+NikDimer@users.noreply.github.com>
2022-11-16 22:42:04 +0300

    v1.3

--- src/components/Book/Book.jsx
+++ src/components/Book/Book.jsx
@@ -1,8 +1,9 @@
-import {useState} from "react";
+import { useState } from 'react';
 import classnames from 'classnames';
 
 import styles from './styles.module.css';
 
+<<<<<<< Updated upstream
 export const Book = (props) => {
     const [counter, setCounter] = useState(0);
 
@@ -20,5 +21,56 @@ export const Book = (props) => {
             <p className={classnames(styles.counter)}>{counter}</p>
             <button className={classnames(styles.plusButton)} onClick={() => setCounter(counter + 1)}></button>
         </div>
+=======
+import { useDispatch, useSelector } from 'react-redux';
+import { selectBookById } from '../../store/book/selectors';
+import { selectBookCount } from '../../store/cart/selectors';
+import { cartSlice } from '../../store/cart';
+
+export const Book = ({ bookId, info }) => {
+  const dispatch = useDispatch();
+  const book = useSelector((state) => selectBookById(state, bookId));
+  const count = useSelector((state) => selectBookCount(state, bookId));
+
+  if (!book) {
+    return null;
+  }
+
+  return (
+    <div
+      className={classnames(styles.book, { [styles.BookPageBook]: info })}
+      key={book.id}
+    >
+      <div className={classnames(styles.bookInfoPart)}>
+        <h3 className={classnames(styles.bookTitle)}>{book.name}</h3>
+        <p className={classnames(styles.bookInfo)}>{book.authors}</p>
+        <p className={classnames(styles.bookInfo)}>{book.genre}</p>
+        <p className={classnames(styles.bookInfo)}>Оценка: {book.rating}</p>
+        <div className={classnames(styles.bookPrice)}>
+          {book.price !== undefined ? (
+            <p>
+              {book.price}
+              <span className={classnames(styles.roubleFont)}>₽</span>
+            </p>
+          ) : (
+            <p>Нет в наличии</p>
+          )}
+        </div>
+      </div>
+
+      <div className={classnames(styles.counterGroup)}>
+        <button
+          className={classnames(styles.minusButton)}
+          onClick={() => dispatch(cartSlice.actions.removeBook(bookId))}
+          disabled={count === 0}
+        ></button>
+        <p className={classnames(styles.counter)}>{count || 0}</p>
+        <button
+          className={classnames(styles.plusButton)}
+          onClick={() => dispatch(cartSlice.actions.addBook(bookId))}
+        ></button>
+      </div>
+>>>>>>> Stashed changes
     </div>
-    }
+  );
+};
--- src/components/CategoryMenu/CategoryMenu.jsx
+++ src/components/CategoryMenu/CategoryMenu.jsx
@@ -1,4 +1,5 @@
 import classnames from 'classnames';
+<<<<<<< Updated upstream
 
 import styles from './styles.module.css';
 
@@ -9,3 +10,30 @@ export const CategoryMenu = (props) => {
                 }
       </nav>
     }
+=======
+import { useSelector } from 'react-redux';
+import { selectCategories } from '../../store/category/selectors';
+
+import styles from './styles.module.css';
+
+export const CategoryMenu = ({ setActiveCategory, activeCategory }) => {
+  const categories = useSelector((state) => selectCategories(state));
+
+  return (
+    <ul className={classnames(styles.mainNavigation)}>
+      {categories.length > 0 &&
+        categories.map((category) => (
+          <li
+            className={classnames(
+              styles.mainNavigationItem, `${category.id === activeCategory && styles.activeCategory}`
+            )}
+            key={category.id}
+            onClick={() => setActiveCategory(category.id)}
+          >
+            {category.name}
+          </li>
+        ))}
+    </ul>
+  );
+};
+>>>>>>> Stashed changes
